b8aeac5a5bbcc992a6f072c26770c8d277228180
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2018-08-29 17:09:18 +0200

    Updated test script: run options, etc.

--- docker-tests.sh
+++ docker-tests.sh
@@ -2,11 +2,23 @@
 #
 # Author: Bert Van Vreckem <bert.vanvreckem@gmail.com>
 #
-# Runs tests for this Ansible role on a Docker container
-# Environment variables DISTRIBUTION and VERSION must be set
-# outside of the script, e.g.
+# Usage: DISTRIBUTION=<distro> VERSION=<version> ./docker-tests.sh
+#
+# Creates a Docker container for the specified Linux distribution and version,
+# available at https://hub.docker.com/r/bertvv/ansible-testing/; runs a syntax
+# check; applies this role to the container using a test playbook; and,
+# finally, runs an idempotence test.
+#
+# Environment variables DISTRIBUTION and VERSION must be set outside of the
+# script.
+#
+# EXAMPLES
 #
 # $ DISTRIBUTION=centos VERSION=7 docker-tests.sh
+# $ DISTRIBUTION=fedora VERSION=28 docker-tests.sh
+# $ DISTRIBUTION=debian VERSION=9 docker-tests.sh
+# $ DISTRIBUTION=ubuntu VERSION=18.04 docker-tests.sh
+#
 
 #{{{ Bash settings
 # abort on nonzero exitstatus
@@ -21,14 +33,16 @@ readonly script_name=$(basename "${0}")
 readonly script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 IFS=$'\t\n'   # Split on newlines and tabs (but not on spaces)
 
+# Color definitions
+readonly reset='\e[0m'
+readonly red='\e[0;31m'
+readonly yellow='\e[0;33m'
+
+# Test environment
 readonly container_id="$(mktemp)"
 readonly role_dir='/etc/ansible/roles/role_under_test'
 readonly test_playbook="${role_dir}/docker-tests/test.yml"
 
-# geerlingguy
-#readonly docker_image="geerlingguy/docker"
-#readonly image_tag="${docker_image}-${DISTRIBUTION}${VERSION}-ansible"
-# bertvv
 readonly docker_image="bertvv/ansible-testing"
 readonly image_tag="${docker_image}:${DISTRIBUTION}_${VERSION}"
 
@@ -46,37 +60,37 @@ main() {
   run_test_playbook
   run_idempotence_test
 
-  # Uncomment the following line if you want to clean up the
-  # container(s) after running the tests. *Not* cleaning up may be
-  # useful for troubleshooting
+  # Uncomment the following line if you want to clean up the container(s) after
+  # running the tests. *Not* cleaning up may be useful for troubleshooting or
+  # running functional tests afterwards.
 
   # cleanup
 }
 
 #{{{ Helper functions
 
-# Usage: configure_environment.
+# Usage: configure_environment
 #
 # This function defines run options for Docker, depending on the
 # Linux $DISTRIBUTION and $VERSION.
 configure_environment() {
 
   case "${DISTRIBUTION}_${VERSION}" in
-    'centos_6')
+    centos_6)
       run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
       ;;
-    'centos_7'|'fedora_25')
+    centos_7|fedora_*)
       init=/usr/lib/systemd/systemd
       run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
       ;;
-    'ubuntu_14.04')
+    ubuntu_14.04)
       #run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
       # Workaround for issue when the host operating system has SELinux
       if [ -x '/usr/sbin/getenforce' ]; then
         run_opts+=('--volume=/sys/fs/selinux:/sys/fs/selinux:ro')
       fi
       ;;
-    'ubuntu_16.04')
+    ubuntu_1[68].04|debian_*)
       run_opts=('--volume=/run' '--volume=/run/lock' '--volume=/tmp' '--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro' '--cap-add=SYS_ADMIN' '--cap-add=SYS_RESOURCE')
 
       if [ -x '/usr/sbin/getenforce' ]; then
@@ -84,7 +98,7 @@ configure_environment() {
       fi
       ;;
     *)
-      log "Warning: no run options added for ${DISTRIBUTION} ${VERSION}"
+      warn "Warning: no run options added for ${DISTRIBUTION} ${VERSION}"
       ;;
   esac
 }
@@ -118,7 +132,7 @@ get_container_id() {
 # Prints the IP address of the specified container.
 get_container_ip() {
   local id
-  id="$(get_docker_id)"
+  id="$(get_container_id)"
 
   docker inspect \
     --format '{{ .NetworkSettings.IPAddress }}' \
@@ -182,12 +196,20 @@ cleanup() {
   rm "${container_id}"
 }
 
+# Usage: log [ARG]...
+#
+# Prints all arguments on the standard output stream
 log() {
-  local yellow='\e[0;33m'
-  local reset='\e[0m'
-
-  printf "${yellow}>>> %s${reset}\n" "${*}"
+  printf "${yellow}>>> %s${reset}\\n" "${*}"
 }
+
+# Usage: warn [ARG]...
+#
+# Prints all arguments on the standard error stream
+warn() {
+  printf "${red}!!! %s${reset}\\n" "${*}" 1>&2
+}
+
 
 #}}}
 
